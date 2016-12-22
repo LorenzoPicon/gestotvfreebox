94ad2b60ba8abd0ff449a60ff4a615e53a87e743
Serveur Nextcloud <myboxy@mail.myboxy.cloud>
2016-12-22 19:58:07 +0100

    Maj

---
+++ config.xxx/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=[Nom de la Base]',
+    'username' => '[Utilisateur]',
+    'password' => '[Mot de Passe]',
+    'charset' => 'utf8',
+];
